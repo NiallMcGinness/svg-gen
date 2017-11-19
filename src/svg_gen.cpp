8818e3284534852b3e7de77d2bd6a4af11b9f8bf
Niall McGinness <njwmcginness@gmail.com>
2017-11-18 18:31:27 -0800

    added svg header to main

--- src/svg_gen.cpp
+++ src/svg_gen.cpp
@@ -1,1 +1,16 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+    //ofstream outputFile;
+    //outputFile.open('out.svg');
+
+    string svg_header ("<svg width='512' height='512' viewBox='0 0 512 512' xmlns='http://www.w3.org/2000/svg'>");
+    string svg_escape ("</svg>");
+    cout<< svg_header << endl;
+
+}
